c8c7e0e87f5fbce9bc63abf5c11a57b3d86f28a9
Super Best User <Oleg@my.com>
2017-11-16 17:20:59 +0300

    start dev

---
+++ pythonLists.py
@@ -0,0 +1,34 @@
+# Python3 Lists
+print('')
+print('Python3 Lists. Create')
+guests = ['ross', 'joey', 'chandler', 'monica', 'rachel', 'phoebe']
+print(guests)
+print('')
+
+massage = "There is my lovely friend " +guests[1].title() + "!"
+print(massage)
+print('')
+
+# Edit List
+# Add elemenent
+print('Edit List. Add elemenent by APPEND')
+guests.append('janice')
+print(guests)
+print('')
+
+print('Edit List. Add elemenent by INSERT')
+guests.insert(0, 'richard')
+print(guests)
+print('')
+
+# Delete elements
+print('Edit List. Delete elements by DEL')
+del guests[0]
+print(guests)
+print('')
+
+print('Edit List. Hide elements by POP()')
+notFriend = guests.pop() #possible to set index. Default is the last one.
+print(guests)
+print('\t' + notFriend.title() + ' is not a friend!')
+print('')
